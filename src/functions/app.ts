bf1e517cb9d1e112bbb76fd542fe38a41233c0f9
Chris Campbell <808531+ctcampbell@users.noreply.github.com>
2023-11-09 22:13:39 +0000

    Make it work

--- src/functions/app.ts
+++ src/functions/app.ts
@@ -1,10 +1,9 @@
 import { Probot } from "probot";
 
 export = (app: Probot) => {
-  app.onAny(async (context) => {
-    context.log(context.payload.installation.id);
-    const oktokit = await auth(context.payload.installation.id);
-    await oktokit.rest.repos.createDispatchEvent({
+  app.onAny(async (context: any) => {
+    const octokit = await app.auth(context.payload.installation.id);
+    await octokit.repos.createDispatchEvent({
       owner: context.payload.repository.owner.login,
       repo: context.payload.repository.name,
       event_type: context.name  
