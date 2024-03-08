d7fc7ec9d9354cf756e21460c473bff47ad308ca
aegilops <41705651+aegilops@users.noreply.github.com>
2024-03-08 16:19:47 +0000

    Update lists of files

--- make-function-zip.sh
+++ make-function-zip.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # Make the Function zip archive for deployment to the Azure Functions App
 
-npm run build
-zip -r teams-secret-scanning-notifier.zip . \
+rm teams-secret-scanning-notifier.zip >/dev/null 2>&1 || echo "[.] No existing zip file to remove"
+npm run build >/dev/null 2>&1
+zip -q -r teams-secret-scanning-notifier.zip . \
 -x 'teams-secret-scanning-notifier.zip' '.vscode/*' '*settings*.json' \
     '.git/*' '.github/*' '.gitignore' '.eslint*' '.funcignore' '*.md' \
     'CODEOWNERS' 'LICENSE' '*.ts' '*.map' 'jest.config.js' \
     '*.example' 'node_modules/.bin/*' '*.sh' '*.env' \
-    'tsconfig.json' 'package-lock.json' \
-    'package.json' 'src/*' 'test/*' 'dist/tsconfig.tsbuildinfo' \
+    'tsconfig.json' 'src/*' 'test/*' \
     'node_modules/@types/*' 'node_modules/*jest*/*' \
     'node_modules/*eslint*/*' '*/.github/*' '*/.history/*' \
     '*/.travis.yml' '*/.eslintignore' '*/.eslintrc.yml' \
-    '*/.prettierignore' '*/.prettierrc.yml' '*/.nycrc' \
     '*/.jshintignore' '*/.jshintrc' '*/.npmignore' \
+    '*/.prettierignore' '*/.prettierrc.yml' '*/.nycrc' \
     'node_modules/*/@types/*' 'node_modules/*/*jest*/*' \
     '*/LICENSE*' '*/license' '*/.eslintrc' '*/.prettierrc' \
     '*/diagnosticMessages.generated.json' '*/tsconfig.json' \
