090938bda7ff27ca14b2b442d9e8102fefa2b552
aegilops <41705651+aegilops@users.noreply.github.com>
2024-03-08 14:20:48 +0000

    Using temp file

--- sync-settings-local-to-remote.sh
+++ sync-settings-local-to-remote.sh
@@ -4,8 +4,10 @@
 
 az account set --subscription "${AZURE_SUBSCRIPTION_ID}"
 
-# extract the settings from the local.settings.json file with jq - just pull out the "values" key as the whole new JSON file
-jq -r '.Values' local.settings.json > local.settings.values.json
+tmp_file=$(mktemp)
 
-az functionapp config appsettings set --name "${AZURE_FUNCTION_APP_NAME}" --resource-group "${AZURE_RESOURCE_GROUP}" --settings @local.settings.values.json
+# extract the settings from the local.settings.json file with jq - just pull out the "values" key as the whole new JSON file
+jq -r '.Values' local.settings.json > "$tmp_file"
+
+az functionapp config appsettings set --name "${AZURE_FUNCTION_APP_NAME}" --resource-group "${AZURE_RESOURCE_GROUP}" --settings @"$tmp_file"
 
