bbfb9fa3288260a52a6b4220a3c63e80e718251c
aegilops <41705651+aegilops@users.noreply.github.com>
2024-03-15 11:04:33 +0000

    Fixed script path

--- scripts/deploy-zip.sh
+++ scripts/deploy-zip.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 . "${SCRIPT_DIR}"/azure.env
 
-./make-function-zip.sh
+"${SCRIPT_DIR}"/make-function-zip.sh
 
 az account set --subscription "${AZURE_SUBSCRIPTION_ID}"
 az functionapp deployment source config-zip --resource-group "${AZURE_RESOURCE_GROUP}" --name "${AZURE_FUNCTION_APP_NAME}" --src teams-secret-scanning-notifier.zip
