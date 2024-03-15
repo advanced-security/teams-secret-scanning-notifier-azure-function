2b5c519c7043bbf794de0933b1e7265baa156e6c
aegilops <41705651+aegilops@users.noreply.github.com>
2024-03-15 17:08:19 +0000

    Updated scripts to use npm install

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 . "${SCRIPT_DIR}"/azure.env
 
+npm install
 npm run build
 func azure functionapp publish "${AZURE_FUNCTION_APP_NAME}" || echo "Failed to publish function app, try running 'az logout' then 'az login', then try again"
--- scripts/make-function-zip.sh
+++ scripts/make-function-zip.sh
@@ -5,7 +5,10 @@ set -euo pipefail
 # Make the Function zip archive for deployment to the Azure Functions App
 
 rm teams-secret-scanning-notifier.zip >/dev/null 2>&1 || echo "[.] No existing zip file to remove"
+
+npm install >/dev/null 2>&1
 npm run build >/dev/null 2>&1
+
 zip -q -r teams-secret-scanning-notifier.zip . \
 -x 'teams-secret-scanning-notifier.zip' '.vscode/*' '*settings*.json' \
     '.git/*' '.github/*' '.gitignore' '.eslint*' '.funcignore' '*.md' \
