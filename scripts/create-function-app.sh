3fc8d690366ee973ef47d8b21f6a16c4c6fbfa77
aegilops <41705651+aegilops@users.noreply.github.com>
2024-03-25 11:53:14 +0000

    Added Owner tag to Azure Function and resources

--- scripts/create-function-app.sh
+++ scripts/create-function-app.sh
@@ -10,6 +10,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 AZURE_STORAGE_ACCOUNT="${AZURE_STORAGE_ACCOUNT:-teamssecretnotifier001sa}"
 
 az account set --subscription "${AZURE_SUBSCRIPTION_ID}"
-az group create --name "${AZURE_RESOURCE_GROUP}" --location "${AZURE_LOCATION}"
-az storage account create --name "${AZURE_STORAGE_ACCOUNT}" --location "${AZURE_LOCATION}" --resource-group "${AZURE_RESOURCE_GROUP}" --sku Standard_LRS
-az functionapp create --resource-group "${AZURE_RESOURCE_GROUP}" --consumption-plan-location "${AZURE_LOCATION}" --runtime node --runtime-version 20 --functions-version 4 --name "${AZURE_FUNCTION_APP_NAME}" --storage-account "${AZURE_STORAGE_ACCOUNT}"
+az group create --name "${AZURE_RESOURCE_GROUP}" --location "${AZURE_LOCATION}" --tags Owner="$(id -un)"
+az storage account create --name "${AZURE_STORAGE_ACCOUNT}" --location "${AZURE_LOCATION}" --resource-group "${AZURE_RESOURCE_GROUP}" --sku Standard_LRS --tags Owner="$(id -un)"
+az functionapp create --resource-group "${AZURE_RESOURCE_GROUP}" --consumption-plan-location "${AZURE_LOCATION}" --runtime node --runtime-version 20 --functions-version 4 --name "${AZURE_FUNCTION_APP_NAME}" --storage-account "${AZURE_STORAGE_ACCOUNT}" --tags Owner="$(id -un)"
